ff7b6db6b0f2acfb4d906dd746e20344cb033bc6
Natalia <natalia80@gmail.com>
2018-04-01 15:05:50 +0200

    Download html method is working

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from vaacscraper import VAACScraper
 
+from datetime import datetime
+
 output_file = "dataset.csv"
 
-
+idate=datetime(2018,3,31)
+edate=datetime.utcnow()
+idate.isoformat(' ')
+edate.isoformat(' ')
 scraper = VAACScraper(idate,edate);
 scraper.scraping();
 scraper.write_csv(output_file);
