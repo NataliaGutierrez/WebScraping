e7e48df8566f85fee9ffa5f40e3acab15133388e
Natalia <natalia80@gmail.com>
2018-04-03 21:44:07 +0200

    Added some code about crawling links for advisories.

--- src/vaacscraper.py
+++ src/vaacscraper.py
@@ -18,6 +18,7 @@ class VAACScraper():
         self.rp = urllib.robotparser.RobotFileParser()
         self.rp.set_url(self.domain + "robots.txt")
         self.rp.read()
+        self.volcanoes = []
     def __checking_useragent(self, url):
         return self.rp.can_fetch(self.useragent, url)
     
@@ -47,6 +48,7 @@ class VAACScraper():
         for year in years:
             tmp = soup.find(name='a',text=str(year))
             if tmp:
+                # TODO: path absoluto, comprobar relativo a website o root local(self.url)
                 ylinks.append(tmp.get('href'))
             else:
                 print('there is no link for year %d',year)
@@ -55,6 +57,10 @@ class VAACScraper():
         # webpage for current year has different format. 
         if datetime.utcnow().year == years[0]:
             # Search in special format
+            url = self.domain + ylinks[idx]
+            html = self.__download_html(url)
+            
+            # TODO: completar a partir del usado en el loop.
             
             # The general loop will be from second element, set initial index
             fidx = 1
@@ -62,38 +68,58 @@ class VAACScraper():
             # THe general loop will be from first element, set initial index
             fidx = 0
         
+        mylinks = []
         for idx in range(fidx,len(ylinks),1):
+            # TODO: ir actualizando self.url con el link donde estoy
             url = self.domain + ylinks[idx]
+            root = []
+            # No hace falta root, conseguirlo desde self.url que siempre estara actualizado.
+            # POner update del self.url primera linea de download_html. 
             html = self.__download_html(url)
             tags = soup.find_all("table")
+            # In last table there are the data
             cols = tags[-1].find_all('td',attrs={"valign": "top"})
             for col in cols:
                 valid = False
                 for child in col.children:
-                    if (child.name=='dt'):
-                        # Child is name of section
-                        # Check wrong section
-                        title = child.find('em').get_text().lower()
-                        if ('user message' in title)|('test ' in title):
-                            valid= False
-                        else:
-                            valid= True
-                    if (child.name=='dd'&valid):
+                    if (child.name=='dt'): # Child is name of section
+                        title = child.find('em').get_text()
+                        # If section is wrong section, next sdvisry record will 
+                        # not be read. Also filter by volcano if there is input 
+                        # list
+                        valid = self.__check_advisories_section( title )
+                        
+                    if (child.name=='dd'&valid): # Child is advisory record
                         tmp = child.find('em').get_text() + child.find('a').get_text()
                         mydate=datetime.fromtimestamp(mktime(strptime(tmp,"%d %b %Y - %H%M UTC")))
-                        if (mydate>idate & mydate<edate):
-                             child.find('a').get('href')
-                                
-                            
-                        
-                        
-                
-            
-        
+                        # Filter by date
+                        if (mydate>self.idate & mydate<self.edate):
+                             mylinks.append(self.__absolute_ref( child.find('a').get('href'), root ))
+                             
+
         # Del html descarregat, trobar els links d'on descarregar els volcanic 
         # ash advisories.
         # Només descarregar els informes dins de l'interval de temps (self.idate,self.edate)
-        return ylinks
+        return mylinks
+    def __absolute_ref( relativelink, root ):
+        # Starting by / character, it is relative to website
+        if (relativelink[0] == '/'):
+            return self.domain + relativelink[1:]
+        # otherwise, it is relative to local root
+        else:
+            return root + relativelink
+    def __check_advisories_section( title ):
+        name = title.lower()
+        
+        if len(self.volcanoes)>0:
+            for volcano in self.volcanoes:
+                if (volcano.lower() in name):
+                    return True
+            return False
+        if ('user message' in name)|('test ' in name):
+            return False
+        return True
+    
     def __scraping_advisory(self,html):
         # Extreure les dades interessants. Insertar registre en dataframe
         a=1
